b742dd38e4e19aa29c6dac966f36cce76ac47a89
Jeff Kwiat <j@jeffkwiat.com>
2017-04-05 13:43:43 -0700

    check-in before ReactJS

--- web/app.py
+++ web/app.py
@@ -1,7 +1,6 @@
-import psycopg2
-
-from flask import Flask, render_template
-from models import db
+from flask import flash, Flask, render_template, redirect, request, session, url_for
+from werkzeug.security import generate_password_hash, check_password_hash
+from models import db, User
 
 app = Flask(__name__)
 
@@ -12,13 +11,74 @@ app.config.from_object('config')
 db.init_app(app)
 db.create_all(app=app)
 
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/signup')
+@app.route('/favorites')
+def favorites():
+    return render_template('index.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    if request.method == 'POST':
+
+        # get the user details from the form
+        email = request.form['email']
+        username = request.form['username']
+        password = request.form['password']
+
+        # hash the password
+        password = generate_password_hash(password)
+
+        user = User(email=email, username=username, password=password)
+
+        db.session.add(user)
+        db.session.commit()
+
+        flash('Thanks for signing up please login')
+
+        return redirect(url_for('home'))
+
+    # it's a GET request, just render the template
     return render_template('signup.html')
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    # we don't need to check the request type as flask will raise a bad request
+    # error if a request aside from POST is made to this url
+
+    username = request.form['username']
+    password = request.form['password']
+
+    # search the database for the User
+    user = User.query.filter_by(username=username).first()
+
+    if user:
+        password_hash = user.password
+
+        if check_password_hash(password_hash, password):
+            # The hash matches the password in the database log the user in
+            session['user'] = username
+
+            flash('Login was succesfull')
+    else:
+        # user wasn't found in the database
+        flash('Username or password is incorrect please try again', 'error')
+
+    return redirect(url_for('home'))
+
+
+@app.route('/logout')
+def logout():
+    if 'user' in session:
+        session.pop('user')
+
+        flash('We hope to see you again!')
+
+    return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.run()
